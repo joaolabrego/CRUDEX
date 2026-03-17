c0ea865a096af346b6b51ff074a6922302ee850b
João Rocha Labrego <joaolabrego@gmail.com>
2026-03-16 21:35:38 -0300

    Atualização

--- CRUDEX/Classes/Reports.cs
+++ CRUDEX/Classes/Reports.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Drawing.Wordprocessing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using Newtonsoft.Json.Linq;
